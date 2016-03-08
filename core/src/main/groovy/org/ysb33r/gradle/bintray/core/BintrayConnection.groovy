cd834b78bbbadd746de4b8065f008e1e612eaae7
Jerry Wiltse <jerry.wiltse@getgsi.com>
2016-03-08 11:35:21 -0500

    Commented out debug println's

--- core/src/main/groovy/org/ysb33r/gradle/bintray/core/BintrayConnection.groovy
+++ core/src/main/groovy/org/ysb33r/gradle/bintray/core/BintrayConnection.groovy
@@ -52,9 +52,9 @@ class BintrayConnection {
 //            apiClient().setProxy('localhost', 8888, null)
 //            apiClient().ignoreSSLIssues()
 //        }
-        println "Endpoint is: $endpoint.text"
-        println "Method is: $method"
-        println "Request Args are: $requestArgs"
+//        println "Endpoint is: $endpoint.text"
+//        println "Method is: $method"
+//        println "Request Args are: $requestArgs"
         try {
             HttpResponseDecorator response = getClient(endpoint)."$method"(requestArgs)
             debugmsg "Response code is ${response.status}."
